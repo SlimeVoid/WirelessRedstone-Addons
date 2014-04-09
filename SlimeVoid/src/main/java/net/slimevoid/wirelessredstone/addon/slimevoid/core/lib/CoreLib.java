0e9041f0d11fc1ec28c9b96acda0a393519e007b
Eurymachus <reflex_ion@hotmail.com>
2014-04-09 10:12:31 +0100

    Fixed build scripts and dependency management

--- SlimeVoid/src/main/java/net/slimevoid/wirelessredstone/addon/slimevoid/core/lib/CoreLib.java
+++
@@ -1,9 +0,0 @@
-package net.slimevoid.wirelessredstone.addon.slimevoid.core.lib;
-
-public class CoreLib {
-
-    public static final String MOD_ID           = "WirelessSlimeVoid";
-    public static final String MOD_NAME         = "Wireless Redstone - Wireless Slimevoid Additions";
-    public static final String MOD_VERSION      = "1.0.0.0";
-    public static final String MOD_DEPENDENCIES = "required-after:WirelessRedstoneCore";
-}
