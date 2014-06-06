c623a9e3654ed7975ebed8d4187c556e7d54c8c3
Eurymachus <reflex_ion@hotmail.com>
2014-06-06 14:35:47 +0100

    Build version update

--- Camouflage/src/main/java/net/slimevoid/wirelessredstone/addon/camouflager/core/lib/CoreLib.java
+++ Camouflage/src/main/java/net/slimevoid/wirelessredstone/addon/camouflager/core/lib/CoreLib.java
@@ -2,12 +2,13 @@ package net.slimevoid.wirelessredstone.addon.camouflager.core.lib;
 
 public class CoreLib {
 
-    public static final String MOD_ID           = "WirelessCamouflager";
-    public static final String MOD_NAME         = "Wireless Redstone - Wireless Camouflager";
-    public static final String MOD_VERSION      = "1.0.0.0";
-    public static final String MOD_DEPENDENCIES = "required-after:WirelessRedstoneCore";
-    public static final String MOD_RESOURCES    = "camouflager";
-    public static final String CLIENT_PROXY     = "net.slimevoid.wirelessredstone.addon.camouflager.client.proxy.CamouClientProxy";
-    public static final String COMMON_PROXY     = "net.slimevoid.wirelessredstone.addon.camouflager.proxy.CamouCommonProxy";
+    public static final String MOD_ID              = "WirelessCamouflager";
+    public static final String MOD_NAME            = "Wireless Redstone - Wireless Camouflager";
+    public static final String MOD_VERSION         = "1.0.0.0";
+    public static final String MOD_DEPENDENCIES    = "";
+    public static final String ACTUAL_DEPENDENCIES = "required-after:WirelessRedstoneCore@[@WRVERSION@,)";
+    public static final String MOD_RESOURCES       = "camouflager";
+    public static final String CLIENT_PROXY        = "net.slimevoid.wirelessredstone.addon.camouflager.client.proxy.CamouClientProxy";
+    public static final String COMMON_PROXY        = "net.slimevoid.wirelessredstone.addon.camouflager.proxy.CamouCommonProxy";
 
 }
